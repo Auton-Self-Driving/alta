4a238c35431cb112f646c454284715bf41076f0a
Shubhankar Deshpande <shubhankardeshpande@hotmail.com>
2019-02-25 22:54:35 -0500

    Added basic step and reset functions

--- environment/carla/env.py
+++ environment/carla/env.py
@@ -9,6 +9,8 @@ from datetime import datetime
 import os
 import glob
 import sys
+import traceback
+import random
 
 CARLA_PATH = os.environ.get("CARLA_PATH")
 if CARLA_PATH == None:
@@ -59,7 +61,10 @@ DEFAULT_ENV = {
     "num_vehicles" : 0,
     "num_pedestrians" : 0,
     "max_steps" : 1000,
-    "next_command": None
+    "next_command": None,
+    "verbose": True
+    "vehicle_type": 'vehicle.toyota.prius',
+    "sensors": ["r"]
 }
 
 DISCRETE_ACTIONS = {
@@ -108,8 +113,8 @@ class CarlaEnv(gym.Env):
         self.server_port = config["server_port"]
         self.city_name = config["city_name"]
         # TODO: Check planner API from 0.9
-        if self.config["enable_planner"]:
-            self.planner = Planner(self.city_name)
+        # if self.config["enable_planner"]:
+        #     self.planner = Planner(self.city_name)
         
         if config["discrete_actions"]:
             self.action_space = Discrete(len(DISCRETE_ACTIONS))
@@ -137,12 +142,75 @@ class CarlaEnv(gym.Env):
         self.start_pos = None
         self.end_pos = None
 
+    def spawn_client(self, hostname='localhost', port_number=None):
+        
+        return client
 
     def step(self, action):
-        pass
+        try:
+            obs = self.step_(action)
+            return obs
+        except Exception:
+            print("Error during step, terminating episode early",
+        traceback.format_exc())
+
+    def _step(self, action):
+        if(self.config['discrete_actions']):
+            action = DISCRETE_ACTIONS[int(action)]
+        throttle = float(np.clip(action[0], 0, 1))
+        brake = float(np.abs(np.clip(action[0], -1, 0)))
+        steer = float(np.clip(action[1], -1, 1))
+        reverse = False
+        hand_brake = False
+
+        #Print actions
+        if self.config['verbose']:
+            print("steer", steer, "throttle", throttle, "brake", brake,
+                  "reverse", reverse)
+        
+        #Send action to agent
     
     def reset(self):
         pass
+
+    def _reset(self):
+        self.num_steps = 0
+        self.total_reward = 0
+        self.prev_measurement = None
+        self.prev_image = None
+        self.episode_id = datetime.today().strftime("%Y-%m-%d_%H-%M-%S_%f")
+        self.measurements_file = None
+
+        client = carla.Client(hostname, port_number)
+
+        world = client.get_world()
+        blueprint_library = world.get_blueprint_library()
+        try:
+            vehicle_bp = blueprint_library.find(self.config['vehicle_type'])
+        except Exception as e:
+            print("Error during vehicle creation: {}".format(traceback.format_exc()))
+        
+        #Returns a list of carla.libcarla.Transform
+        spawn_points = world.get_map().get_spawn_points()
+        #carla.libcarla.Transform has attributes location, rotation
+        spawn_point = random.choice(spawn_points)
+        
+        vehicle_actor = world.spawn_actor(vehicle_bp, spawn_point)
+
+        #TODO: Generalize this code to attach 'n' different sensors to the vehicle
+        #Attach a sensor to the vehicle
+        sensor = self.config['sensors'][0]
+        camera = blueprint_library.find(sensor)
+        camera_transform = carla.Transform(carla.Location(x=1.5, z=2.4))
+        camera_actor = world.spawn_actor(camera, camera_transform, attach_to=vehicle_actor)
+        if(self.config['save_images_to_disk']):
+            camera_actor.listen(lambda image: image.save_to_disk('output/%06d.png' % image.frame_number))
+        # Get start and end positions (to figure out when to end the episode)
+        # print("Start pos {}, End Pos {}".format(
+        #     spawn_point.location, self.start_coord,
+        #     self.scenario["end_pos_id"], self.end_coord))
+
+
 
     def _read_data(self):
         pass
