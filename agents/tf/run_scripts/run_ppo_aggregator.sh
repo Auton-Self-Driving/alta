f60b746008cbac409e6f7f7c23dcf317f5396b1e
agarwaltanmay <tanmay.omshanti.agrawal@gmail.com>
2019-11-01 22:33:01 -0400

    Adding PPO measurement run scripts, delete old models

---
+++ agents/tf/run_scripts/run_ppo_aggregator.sh
@@ -0,0 +1,18 @@
+# python /zfsauton2/home/tanmaya/projects/tensorboard-aggregator/aggregator.py --path /zfsauton2/home/tanmaya/projects/tb-agg/algo_PPO_lr_0.0002_layers_1_layer_steer_coeff_0_ent_coef_0.005/ &
+# python /zfsauton2/home/tanmaya/projects/tensorboard-aggregator/aggregator.py --path /zfsauton2/home/tanmaya/projects/tb-agg/algo_PPO_lr_0.0002_layers_2_layer_steer_coeff_0_ent_coef_0.005/ &
+# python /zfsauton2/home/tanmaya/projects/tensorboard-aggregator/aggregator.py --path /zfsauton2/home/tanmaya/projects/tb-agg/algo_PPO_lr_0.0002_layers_2_layer_steer_coeff_10.0_ent_coef_0.005/ &
+python /zfsauton2/home/tanmaya/projects/tensorboard-aggregator/aggregator.py --path /zfsauton2/home/tanmaya/projects/tb-agg/algo_PPO_lr_0.0004_layers_1_layer_steer_coeff_0_ent_coef_0.005/ &
+python /zfsauton2/home/tanmaya/projects/tensorboard-aggregator/aggregator.py --path /zfsauton2/home/tanmaya/projects/tb-agg/algo_PPO_lr_0.0004_layers_2_layer_steer_coeff_0_ent_coef_0.005/ &
+python /zfsauton2/home/tanmaya/projects/tensorboard-aggregator/aggregator.py --path /zfsauton2/home/tanmaya/projects/tb-agg/algo_PPO_lr_0.0004_layers_2_layer_steer_coeff_10.0_ent_coef_0.005/ &
+
+
+
+
+# python /zfsauton2/home/tanmaya/projects/tensorboard-aggregator/aggregator.py --path /zfsauton2/home/tanmaya/projects/tb-agg/algo_PPO_lr_0.0002_layers_1_layer_steer_coeff_0_ent_coef_0.005/ &
+# python /zfsauton2/home/tanmaya/projects/tensorboard-aggregator/aggregator.py --path /zfsauton2/home/tanmaya/projects/tb-agg/algo_PPO_lr_0.0002_layers_1_layer_steer_coeff_10.0_ent_coef_0.005/ &
+# python /zfsauton2/home/tanmaya/projects/tensorboard-aggregator/aggregator.py --path /zfsauton2/home/tanmaya/projects/tb-agg/algo_PPO_lr_0.0002_layers_2_layer_steer_coeff_0_ent_coef_0.005/ &
+# python /zfsauton2/home/tanmaya/projects/tensorboard-aggregator/aggregator.py --path /zfsauton2/home/tanmaya/projects/tb-agg/algo_PPO_lr_0.0002_layers_2_layer_steer_coeff_10.0_ent_coef_0.005/ &
+# python /zfsauton2/home/tanmaya/projects/tensorboard-aggregator/aggregator.py --path /zfsauton2/home/tanmaya/projects/tb-agg/algo_PPO_lr_0.0004_layers_1_layer_steer_coeff_0_ent_coef_0.005/ &
+# python /zfsauton2/home/tanmaya/projects/tensorboard-aggregator/aggregator.py --path /zfsauton2/home/tanmaya/projects/tb-agg/algo_PPO_lr_0.0004_layers_1_layer_steer_coeff_10.0_ent_coef_0.005/ &
+# python /zfsauton2/home/tanmaya/projects/tensorboard-aggregator/aggregator.py --path /zfsauton2/home/tanmaya/projects/tb-agg/algo_PPO_lr_0.0004_layers_2_layer_steer_coeff_0_ent_coef_0.005/ &
+# python /zfsauton2/home/tanmaya/projects/tensorboard-aggregator/aggregator.py --path /zfsauton2/home/tanmaya/projects/tb-agg/algo_PPO_lr_0.0004_layers_2_layer_steer_coeff_10.0_ent_coef_0.005/ &
