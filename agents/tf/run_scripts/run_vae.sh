f78a0b9105e1065baf3a76728bce88e30ed842ad
agarwaltanmay <tanmay.omshanti.agrawal@gmail.com>
2019-11-02 00:42:37 -0400

    Updating path in run scripts

--- agents/tf/run_scripts/run_vae.sh
+++ agents/tf/run_scripts/run_vae.sh
@@ -1,5 +1,5 @@
-python run_vae.py --algo VAE --run-id 1 --base-log-dir '/zfsauton2/home/hiteshar/research/alta-logs/new_env_runs/vae_1/' --timesteps 2000000 --lr 5e-3 --num-npc 60 --vae-zsize 64 --code-gpu 1 --carla-gpu 0 &
+python ../run_vae.py --algo VAE --run-id 1 --base-log-dir '/zfsauton2/home/hiteshar/research/alta-logs/new_env_runs/vae_1/' --timesteps 2000000 --lr 5e-3 --num-npc 60 --vae-zsize 64 --code-gpu 1 --carla-gpu 0 &
 sleep 60
-python run_vae.py --algo VAE --run-id 1 --base-log-dir '/zfsauton2/home/hiteshar/research/alta-logs/new_env_runs/vae_1/' --timesteps 2000000 --lr 5e-3 --num-npc 60 --vae-zsize 128 --code-gpu 2 --carla-gpu 0 &
+python ../run_vae.py --algo VAE --run-id 1 --base-log-dir '/zfsauton2/home/hiteshar/research/alta-logs/new_env_runs/vae_1/' --timesteps 2000000 --lr 5e-3 --num-npc 60 --vae-zsize 128 --code-gpu 2 --carla-gpu 0 &
 sleep 60
-python run_vae.py --algo VAE --run-id 1 --base-log-dir '/zfsauton2/home/hiteshar/research/alta-logs/new_env_runs/vae_1/' --timesteps 2000000 --lr 5e-3 --num-npc 60 --vae-zsize 256 --code-gpu 3 --carla-gpu 0 &
+python ../run_vae.py --algo VAE --run-id 1 --base-log-dir '/zfsauton2/home/hiteshar/research/alta-logs/new_env_runs/vae_1/' --timesteps 2000000 --lr 5e-3 --num-npc 60 --vae-zsize 256 --code-gpu 3 --carla-gpu 0 &
