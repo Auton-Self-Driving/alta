86184aeecba3745b15541d6a5f765c2acdc4a76f
audhuang <audreynhuang@gmail.com>
2020-02-09 18:47:43 -0500

    added brake to videos

--- agents/tf/vis_module.py
+++ agents/tf/vis_module.py
@@ -32,12 +32,14 @@ class vis():
         overlay = Image.new('RGBA', image.size, (255,255,255,0))
         draw_overlay = ImageDraw.Draw(overlay)
         draw_overlay.text((10, 10), 
-                          "Sp={:0.3f}\nTSp={:0.3f}\nTh={:0.3f}\nSt={:0.3f}\nBr={:0.3f}\nd={:0.3f}".format(
+                          "Sp={:0.3f}\nTSp={:0.3f}\nTh={:0.3f}\nSt={:0.3f}\nBr={:0.3f}\nObs={:0.3f}\nd={:0.3f}".format(
                               step_info['speed'] * 3.6, 
                               step_info['target_speed'],
                               step_info['control_throttle'], 
                               step_info['control_steer'], 
                               step_info['control_brake'], 
+                              # step_info['obstacle_dist'], 
+                              step_info['obstacle_visible'], 
                               step_info['dist_to_trajectory_reward']), 
                           fill=(255,255,255,128))
         
