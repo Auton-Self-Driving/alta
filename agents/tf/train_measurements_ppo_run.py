2ea81a7ee9b2f434d5408d4b2b55f66ff0065e2a
agarwaltanmay <tanmay.omshanti.agrawal@gmail.com>
2020-02-16 12:12:00 -0500

    Adding SCRATCH_DIR to train_measurements_ppo file.

--- agents/tf/train_measurements_ppo_run.py
+++ agents/tf/train_measurements_ppo_run.py
@@ -19,9 +19,22 @@ from stable_baselines.common.policies import register_policy
 from ppo import PPO, plot_policy_and_value_fns, test
 from models import Policy_1_layer, Policy_2_layer, CustomPolicy1, CustomPolicy2
 
+def get_scratch_dir(base_log_dir):
+    return base_log_dir.split(base_log_dir.split("/home")[0])[1].replace("/home", "/home/scratch")
+
 def run_ppo(args, prefix, config):
     ALTA_LOGS = os.path.join(args.base_log_dir, prefix.split('_runid_')[0], prefix)
     # config.config['LOG_DIR'] = ALTA_LOGS
+    if ALTA_LOGS[-1] != '/':
+        ALTA_LOGS += '/'
+
+    if os.path.exists('/home/scratch'):
+        SCRATCH_DIR = os.path.join(get_scratch_dir(args.base_log_dir), prefix.split('_runid_')[0], prefix)
+    else:
+        SCRATCH_DIR = ALTA_LOGS
+
+    if SCRATCH_DIR[-1] != '/':
+        SCRATCH_DIR += '/'
     
     POLICY_PLOTS = ALTA_LOGS + 'policy_plots/'
     if not os.path.exists(ALTA_LOGS):
@@ -58,8 +71,8 @@ def run_ppo(args, prefix, config):
                 print("Using the pre-initialized seed: {}".format(seed))
                 set_global_seeds(seed)
 
-                IMAGES_PATH = ALTA_LOGS+'final_images_' + config.config["city_name"] + '/'
-                VIDEO_PATH = ALTA_LOGS+'final_videos_' + config.config["city_name"] + '/'
+                IMAGES_PATH = SCRATCH_DIR+'final_images_' + config.config["city_name"] + '/'
+                VIDEO_PATH = SCRATCH_DIR+'final_videos_' + config.config["city_name"] + '/'
                 vis_wrapper = vis_module.vis(IMAGES_PATH, VIDEO_PATH, FRAME_SKIP, videos=config.config["videos"])
                 
                 env = CarlaEnv(config=config.config, vis_wrapper=vis_wrapper, logger=logger, log_dir=ALTA_LOGS)
@@ -80,8 +93,8 @@ def run_ppo(args, prefix, config):
                     f.write("Total Success Episodes: {}".format(str(success_episodes)))
             else:
                 print("Training begins")
-                IMAGES_PATH = ALTA_LOGS+'images/'
-                VIDEO_PATH = ALTA_LOGS+'videos/'
+                IMAGES_PATH = SCRATCH_DIR+'images/'
+                VIDEO_PATH = SCRATCH_DIR+'videos/'
                 vis_wrapper = vis_module.vis(IMAGES_PATH, VIDEO_PATH, FRAME_SKIP, videos=config.config["videos"])
                 
                 env = CarlaEnv(config=config.config, vis_wrapper=vis_wrapper, logger=logger, log_dir=ALTA_LOGS)
