fb158b4039955404e18e6026a96d1e9ec6b21c9e
Hitesh Arora <hiteshar@autobot.vision.cs.cmu.edu>
2020-07-29 23:42:35 -0400

    update plot sh file

--- agents/tf/plot_rewards.sh
+++ agents/tf/plot_rewards.sh
@@ -429,3 +429,111 @@ python plot_combined_expert_target.py \
 --reward-title 'DDQN-Expert-25 Agent: Affect of Optimization Epochs' \
 --fs 2 \
 --inds 1 2 3 &
+
+
+# gpu3 longst
+python plot_combined_longst.py \
+--log-path '/home/scratch/hiteshar/research/alta-logs/thesis_longst' \
+--run-path 'algo_DQN_input_wp_obs_info_speed_steer_ldist_light_nw_1_layer_lr_1e-05_exp_0.1_dynamic_navigation_npc_50_brake_target_freq_10000_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_steer_pen_2.0_fs_2_pid_fs_dqn_n_3_r_norm_16_suc_r_75_const_r_1_optep_1' \
+--success-title 'Success Rate of DDQN Agent in Straight-Dynamic Task' \
+--reward-title 'Cumulative Reward of DDQN Agent in Straight-Dynamic Task' \
+--fs 5 \
+--inds 1 2 3 &
+
+
+python plot_combined.py \
+--log-path '/home/hiteshar/alta-logs/thesis_cddqn_results' \
+--run-path 'algo_DQN_input_wp_obs_info_speed_steer_ldist_light_nw_1_layer_lr_1e-05_exp_0.1_dynamic_navigation_npc_50_brake_target_freq_10000_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_steer_pen_2.0_fs_2_pid_fs_dqn_n_3_r_norm_16_suc_r_75_const_r_1_optep_1' \
+--success-title 'Success Rate (cDDQN Agent)' \
+--reward-title 'Cumulative Reward (cDDQN Agent)' \
+--fs 2 \
+--inds 1 2 3 &
+
+
+gpu6/research/alta-logs/dqn_long_straight_replay3/algo_DQN_input_wp_obs_info_speed_steer_ldist_goal_light_network_1_layer_lr_3e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_use_pid_fs_/algo_DQN_input_wp_obs_info_speed_steer_ldist_goal_light_network_1_layer_lr_3e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_use_pid_fs__runid_1/Validation_qvalue_plots_buffer_old/E_1080_t_390000_i_0_v_40_qvalues_array.npz
+
+
+python plot_qvalues.py \
+--log-path '/home/scratch/hiteshar/research/alta-logs/dqn_long_straight_replay3/algo_DQN_input_wp_obs_info_speed_steer_ldist_goal_light_network_1_layer_lr_3e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_use_pid_fs_/algo_DQN_input_wp_obs_info_speed_steer_ldist_goal_light_network_1_layer_lr_3e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_use_pid_fs__runid_1/Validation_qvalue_plots_buffer_old/' \
+--run-path 'algo_DQN_input_wp_obs_info_speed_steer_ldist_light_nw_1_layer_lr_1e-05_exp_0.1_dynamic_navigation_npc_50_brake_target_freq_10000_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_steer_pen_2.0_fs_2_pid_fs_dqn_n_3_r_norm_16_suc_r_75_const_r_1_optep_1' \
+--success-title 'Success Rate (cDDQN Agent)' \
+--reward-title 'Cumulative Reward (cDDQN Agent)' \
+--fs 2 \
+--inds 1 2 3 &
+
+/run/user/1000/gvfs/sftp:host=lop2.autonlab.org,user=hiteshar/zfsauton2/home/hiteshar/local_scratch5/gpu19/research/alta-logs/E_0_t_0_i_0_v_1_qvalues_array.npz
+/run/user/1000/gvfs/sftp:host=lop2.autonlab.org,user=hiteshar/zfsauton2/home/hiteshar/local_scratch5/gpu19/research/alta-logs/dqn_replayRL1_wo_buffer1/longst_steer_throttle_1/algo_DQN_input_wp_obs_info_speed_steer_ldist_goal_light_network_1_layer_lr_1e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_fs_5_use_pid_fs_/algo_DQN_input_wp_obs_info_speed_steer_ldist_goal_light_network_1_layer_lr_1e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_fs_5_use_pid_fs__runid_3/buffer_training/Validation_qvalue_plots_buffer/E_0_t_0_i_0_v_1_qvalues_array.npz
+
+/run/user/1000/gvfs/sftp:host=lop2.autonlab.org,user=hiteshar/zfsauton2/home/hiteshar/local_scratch5/gpu19/research/alta-logs/dqn_replayRL1_wo_buffer1/longst_steer_throttle_1/algo_DQN_input_wp_obs_info_speed_steer_ldist_goal_light_network_1_layer_lr_1e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_fs_5_use_pid_fs_/algo_DQN_input_wp_obs_info_speed_steer_ldist_goal_light_network_1_layer_lr_1e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_fs_5_use_pid_fs__runid_3/buffer_training/Validation_qvalue_plots_buffer/E_0_t_0_i_0_v_1_qvalues_array.npz
+dqn_replayRL1_wo_buffer1/longst_steer_throttle_1/algo_DQN_input_wp_obs_info_speed_steer_ldist_goal_light_network_1_layer_lr_1e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_fs_5_use_pid_fs_/algo_DQN_input_wp_obs_info_speed_steer_ldist_goal_light_network_1_layer_lr_1e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_fs_5_use_pid_fs__runid_3/buffer_training/Validation_qvalue_plots_buffer
+python plot_average_qvalues.py \
+--log-path '/home/scratch/hiteshar/research/alta-logs/dqn_replayRL1_wo_buffer1/longst_steer_throttle_1/algo_DQN_input_wp_obs_info_speed_steer_ldist_goal_light_network_1_layer_lr_1e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_fs_5_use_pid_fs_/algo_DQN_input_wp_obs_info_speed_steer_ldist_goal_light_network_1_layer_lr_1e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_fs_5_use_pid_fs__runid_3/buffer_training/Validation_qvalue_plots_buffer/' \
+--run-path 'dqn_replayRL1_wo_buffer1/longst_steer_throttle_1/algo_DQN_input_wp_obs_info_speed_steer_ldist_goal_light_network_1_layer_lr_1e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_fs_5_use_pid_fs_/algo_DQN_input_wp_obs_info_speed_steer_ldist_goal_light_network_1_layer_lr_1e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_fs_5_use_pid_fs__runid_3/buffer_training/Validation_qvalue_plots_buffer/' \
+--success-title 'Q-Value and Discounted Returns Across Training' \
+--reward-title 'Cumulative Reward (cDDQN Agent)' \
+--fs 2 \
+--inds 1 2 3 &
+
+python plot_average_qvalues.py \
+--log-path '/home/scratch/hiteshar/research/alta-logs/dqn_long_straight_replay3/algo_DQN_input_wp_obs_info_speed_steer_ldist_goal_light_network_1_layer_lr_3e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_use_pid_fs_/algo_DQN_input_wp_obs_info_speed_steer_ldist_goal_light_network_1_layer_lr_3e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_use_pid_fs__runid_1/Validation_qvalue_plots_buffer/' \
+--run-path 'algo_DQN_input_wp_obs_info_speed_steer_ldist_light_nw_1_layer_lr_1e-05_exp_0.1_dynamic_navigation_npc_50_brake_target_freq_10000_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_steer_pen_2.0_fs_2_pid_fs_dqn_n_3_r_norm_16_suc_r_75_const_r_1_optep_1' \
+--success-title 'Success Rate (cDDQN Agent)' \
+--reward-title 'Cumulative Reward (cDDQN Agent)' \
+--fs 2 \
+--inds 1 2 3 &
+
+
+/run/user/1000/gvfs/sftp:host=lop2.autonlab.org,user=hiteshar/zfsauton2/home/hiteshar/local_scratch5/gpu5/
+python plot_average_qvalues.py \
+--log-path '/home/scratch/hiteshar/research/alta-logs/dqn_lstjunc_steer7_speed_0_20_ac12_improv_fs3_uniform_nn128/lstjunc_steer7_throttle_0_20_ac12/algo_DQN_input_wp_obs_info_speed_steer_ldist_light_nw_1_layer_lr_3e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_steer_pen_2.0_fs_3_pid_fs_dqn_n_3_rew_norm_24_successr_100_constantr_1/algo_DQN_input_wp_obs_info_speed_steer_ldist_light_nw_1_layer_lr_3e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_steer_pen_2.0_fs_3_pid_fs_dqn_n_3_rew_norm_24_successr_100_constantr_1_runid_1/Validation_qvalue_plots_buffer/' \
+--run-path 'algo_DQN_input_wp_obs_info_speed_steer_ldist_light_nw_1_layer_lr_1e-05_exp_0.1_dynamic_navigation_npc_50_brake_target_freq_10000_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_steer_pen_2.0_fs_2_pid_fs_dqn_n_3_r_norm_16_suc_r_75_const_r_1_optep_1' \
+--success-title 'Average Episodic Q-values over Training' \
+--reward-title 'Cumulative Reward (cDDQN Agent)' \
+--fs 2 \
+--inds 1 2 3 &
+
+
+python plot_average_qvalues.py \
+--log-path '/home/scratch/hiteshar/research/alta-logs/cddqn_lstjunc_nav_steer7_speed_0_20_ac12_fs2_ss_nn256_128_64/steer7_throttle_0_20_ac12/algo_DQN_input_wp_obs_info_speed_steer_ldist_light_lr_1e-05_exp_0.1_long_straight_npc_10_target_freq_10000_steer_pen_2.0_fs_2_cDDQN_ss_dqn_n_3_r_norm_16_suc_r_75_const_r_1_optep_1/algo_DQN_input_wp_obs_info_speed_steer_ldist_light_lr_1e-05_exp_0.1_long_straight_npc_10_target_freq_10000_steer_pen_2.0_fs_2_cDDQN_ss_dqn_n_3_r_norm_16_suc_r_75_const_r_1_optep_1_runid_1/Validation_qvalue_plots_buffer/' \
+--run-path 'algo_DQN_input_wp_obs_info_speed_steer_ldist_light_nw_1_layer_lr_1e-05_exp_0.1_dynamic_navigation_npc_50_brake_target_freq_10000_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_steer_pen_2.0_fs_2_pid_fs_dqn_n_3_r_norm_16_suc_r_75_const_r_1_optep_1' \
+--success-title 'Average Episodic Q-values over Training' \
+--reward-title 'Cumulative Reward (cDDQN Agent)' \
+--fs 2 \
+--inds 1 2 3 &
+
+
+python plot_average_qvalues.py \
+--log-path '/home/scratch/hiteshar/research/alta-logs/cddqn_lstjunc_nav_steer7_speed_0_20_ac12_fs2_ss_nn256_128_64/steer7_throttle_0_20_ac12/algo_DQN_input_wp_obs_info_speed_steer_ldist_light_lr_1e-05_exp_0.1_long_straight_npc_10_target_freq_10000_steer_pen_2.0_fs_2_cDDQN_ss_dqn_n_3_r_norm_16_suc_r_75_const_r_1_optep_1/algo_DQN_input_wp_obs_info_speed_steer_ldist_light_lr_1e-05_exp_0.1_long_straight_npc_10_target_freq_10000_steer_pen_2.0_fs_2_cDDQN_ss_dqn_n_3_r_norm_16_suc_r_75_const_r_1_optep_1_runid_3/Validation_qvalue_plots_buffer/' \
+--run-path 'algo_DQN_input_wp_obs_info_speed_steer_ldist_light_nw_1_layer_lr_1e-05_exp_0.1_dynamic_navigation_npc_50_brake_target_freq_10000_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_steer_pen_2.0_fs_2_pid_fs_dqn_n_3_r_norm_16_suc_r_75_const_r_1_optep_1' \
+--success-title 'Average Episodic Q-values over Training' \
+--reward-title 'Cumulative Reward (cDDQN Agent)' \
+--fs 2 \
+--inds 1 2 3 &
+
+
+Average Episodic Q-values over Training
+
+Avg Q-Values
+Avg Discounted Return
+
+E_38911_t_4523927_i_0_v_229_qvalues_array.npz
+
+E_20372_t_1060953_i_0_v_55_qvalues_array.npz
+
+python plot_qvalues.py \
+--log-path '/home/scratch/hiteshar/research/alta-logs/dqn_lstjunc_steer7_speed_0_20_ac12_improv_fs3_uniform_nn128/lstjunc_steer7_throttle_0_20_ac12/algo_DQN_input_wp_obs_info_speed_steer_ldist_light_nw_1_layer_lr_3e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_steer_pen_2.0_fs_3_pid_fs_dqn_n_3_rew_norm_24_successr_100_constantr_1/algo_DQN_input_wp_obs_info_speed_steer_ldist_light_nw_1_layer_lr_3e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_steer_pen_2.0_fs_3_pid_fs_dqn_n_3_rew_norm_24_successr_100_constantr_1_runid_1/Validation_qvalue_plots_buffer/' \
+--run-path 'E_38911_t_4523927_i_0_v_229_qvalues_array.npz' \
+--success-title 'Success Rate (cDDQN Agent)' \
+--reward-title 'Cumulative Reward (cDDQN Agent)' \
+--fs 2 \
+--inds 1 2 3 &
+
+research/alta-logs/cddqn_lstjunc_nav_steer7_speed_0_20_ac12_fs2_ss_nn256_128_64/steer7_throttle_0_20_ac12/algo_DQN_input_wp_obs_info_speed_steer_ldist_light_lr_1e-05_exp_0.1_long_straight_npc_10_target_freq_10000_steer_pen_2.0_fs_2_cDDQN_ss_dqn_n_3_r_norm_16_suc_r_75_const_r_1_optep_1/algo_DQN_input_wp_obs_info_speed_steer_ldist_light_lr_1e-05_exp_0.1_long_straight_npc_10_target_freq_10000_steer_pen_2.0_fs_2_cDDQN_ss_dqn_n_3_r_norm_16_suc_r_75_const_r_1_optep_1_runid_1/Validation_qvalue_plots_buffer/E_42542_t_8229315_i_0_v_301_qvalues_array.npz
+
+python plot_qvalues.py \
+--log-path '/home/scratch/hiteshar/research/alta-logs/dqn_lstjunc_steer7_speed_0_20_ac12_improv_fs3_uniform_nn128/lstjunc_steer7_throttle_0_20_ac12/algo_DQN_input_wp_obs_info_speed_steer_ldist_light_nw_1_layer_lr_3e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_steer_pen_2.0_fs_3_pid_fs_dqn_n_3_rew_norm_24_successr_100_constantr_1/algo_DQN_input_wp_obs_info_speed_steer_ldist_light_nw_1_layer_lr_3e-05_long_straight_npc_50_buffer_1000000_brake_col_3.0_col_sp_3.0_light_3.0_light_sp_3.0_steer_pen_2.0_fs_3_pid_fs_dqn_n_3_rew_norm_24_successr_100_constantr_1_runid_1/Validation_qvalue_plots_buffer/' \
+--run-path 'E_34762_t_3087915_i_0_v_157_qvalues_array.npz' \
+--success-title 'Success Rate (cDDQN Agent)' \
+--reward-title 'Cumulative Reward (cDDQN Agent)' \
+--fs 2 \
+--inds 1 2 3 &
