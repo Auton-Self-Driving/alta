00905c92704515f89aa185716949d8dfca6590cd
Swapnil Pande <swapnil.pande98@gmail.com>
2021-01-19 12:18:27 -0500

    Continued modifying carla interface

--- environment/carla_9_4/carla/carla_interface.py
+++ environment/carla_9_4/carla/carla_interface.py
@@ -1,4 +1,5 @@
 from environment.carla_9_4.carla.server import CarlaServer
+from environment.carla_9_4 import planner
 from environment.carla_9_4.carla.vehicle_manager import VehicleManager
 from abc import ABC
 import time
@@ -56,6 +57,9 @@ class Carla910Interface:
         # Get traffic lights
         self.traffic_actors = self.world.get_actors().filter("*traffic_light*")
 
+        # Actor that we drive
+        self.vehicle_actor = None
+
         print("server_version", self.client.get_server_version())
 
     def _spawn_client(self, hostname='localhost', port_number=None):
@@ -64,6 +68,44 @@ class Carla910Interface:
         client.set_timeout(self.config["client_timeout_seconds"])
 
         return client
+
+
+    def reset(self):
+        # Destroy all actors in the world
+        self.destroy_all_actors()
+
+        # reset our car
+        # TODO Need to get observation after we tick to let vehicle land
+        self.vehicle_actor.reset()
+
+        # Tick for 15 frames to handle car initialization in air
+        for _ in range(15):
+            world_frame = self._world.tick()
+
+        # Create a global planner to generate dense waypoints along route
+        self.global_planner = planner.GlobalPlanner()
+
+        #TODO Move these two steps to the global planner if dense_waypoints not used later
+        self.dense_waypoints  = self.global_planner._trace_route(self._map,
+                                self.source_transform, self.destination_transform)
+
+        self.global_planner.set_global_plan(self.trace_route)
+
+        next_orientation, self.dist_to_trajectory, distance_to_goal_trajec, self.next_waypoints, self.next_wp_angles, self.next_wp_vectors = self.global_planner.get_next_orientation_new(self.vehicle_actor.get_transform())
+
+        ep_measurements = {
+            'next_orientation' : next_orientation,
+            'distance_to_goal_trajec' : self.dist_to_trajectory,
+            'dist_to_trajectory' : self.dist_to_trajectory
+        }
+
+        return ep_measurements
+
+    def destroy_all_actors(self):
+        # raise NotImplementedError()
+        pass
+
+
 
 
 
