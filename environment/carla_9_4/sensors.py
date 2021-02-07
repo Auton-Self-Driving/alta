d016f3744854c8f89ff787ba0ab0cdd828cc105c
Kareido <zackhwang@qq.com>
2021-02-07 15:53:25 -0500

    OBS RADIUS BUG FIX [!]

--- environment/carla_9_4/sensors.py
+++ environment/carla_9_4/sensors.py
@@ -184,7 +184,8 @@ class ObstacleSensor(object):
         bp = world.get_blueprint_library().find('sensor.other.obstacle')
         bp.set_attribute('only_dynamics', 'True')
         bp.set_attribute('distance', str(distance))
-        hit_radius = math.atan(1 / distance) # calculate detection range
+        # hit_radius = math.atan(1 / distance) # calculate detection range
+        hit_radius = 0.5
         bp.set_attribute('hit_radius', str(hit_radius))
         self.sensor = world.spawn_actor(bp, carla.Transform(), attach_to=self._parent)
         # We need to pass the lambda a weak reference to self to avoid circular
